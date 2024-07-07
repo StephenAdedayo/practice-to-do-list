e10388ea4236c94c6e6345014f0a48f3a595aef7
StephenAdedayo <alustephenadedayo@gmail.com>
2024-07-07 16:00:44 +0100

    added a delete all button

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const inputBox = document.getElementById('input-box')
 const listCont = document.getElementById('list-cont')
 const button = document.getElementById('btn')
+const delebtn = document.getElementById('del')
 
-button.onclick = function addTask(){
-    if(inputBox.value === ''){
+button.addEventListener("click", () =>{
+   if (inputBox.value === '') {
         alert('you have to add a task')
-    }else {
+   } else {
         let li = document.createElement('li')
         li.innerHTML = inputBox.value
         listCont.appendChild(li)
+        // deleted.appendChild(listCont)
         let span = document.createElement('span')
         // listCont.insertBefore(li, listCont.getElementsByTagName('li')[0])
         span.innerHTML = '\u00d7'
         li.appendChild(span)
+
+
+        
+
+        // let buttons = document.createElement('button')
+        // buttons.innerHTML = 'Done'
+        // li.appendChild(buttons)
 
         
 
@@ -20,23 +29,31 @@ button.onclick = function addTask(){
 
 inputBox.value = ''
 saveData()
-}
+})
 
 
-listCont.addEventListener("click", function(e){
+listCont.addEventListener("click", (e) => {
         if(e.target.tagName === 'LI'){
             e.target.classList.toggle('checked')
             saveData()
         }
-        else{
-            if(e.target.tagName === 'SPAN'){
+        //  else if(e.target.tagName === "BUTTON"){
+        //     e.target.parentElement.remove()
+        // }
+        else if
+            (e.target.tagName === 'SPAN'){
                 e.target.parentElement.remove()
                 saveData()
             }
             
-        }
+}, false);
+
+delebtn.addEventListener("click", () => {
+   
+    listCont.innerHTML = ''
     
-});
+    saveData()
+})
 
 
 function saveData(){
@@ -49,3 +66,11 @@ function getData(){
 
 getData()
 
+ 
+// let buttons = document.createElement('div')
+// buttons.appendChild(listCont)
+
+// document.body.appendChild(listCont, buttons)
+
+
+// 
